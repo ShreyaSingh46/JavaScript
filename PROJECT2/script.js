54f44c7685ce5a3f4a802703ed5f8ef22067c474
ShreyaSingh46 <ssinghshreyaa19@gmail.com>
2026-05-22 02:20:26 +0530

    PROJECT2: BMI CALCULATOR

--- PROJECT2/script.js
+++ PROJECT2/script.js
@@ -6,9 +6,9 @@ form.addEventListener('submit', function(e){
     const results = (document.querySelector('#results'))
 
     if (isNaN(height) || height <= 0) {
-        results.innerHTML = `Please give a valid height! ${height}`;
+        results.innerHTML = `Please give a valid height ${height}`;
     } else if (isNaN(weight) || weight <= 0) {
-        results.innerHTML = `Please give a valid weight! ${weight}`;
+        results.innerHTML = `Please give a valid weight ${weight}`;
     } else {
         const bmi = (weight / ((height * height)/10000)).toFixed(2)
         //show the result
